1d76b382bc2b9e6edd21c4e083aa96dfe9eddacd
Au Doan Ngoc <Au.DoanNgoc@vn.bosch.com>
2022-12-22 18:54:12 +0700

    update learning

---
+++ render-shader/main.cpp
@@ -0,0 +1,97 @@
+/*
+ *  Copyright 2022 Au Doan Ngoc
+ *
+ *  Author: Au Doan Ngoc
+ *  Date: Dec 21, 2022
+ *  Describe: render a dot
+*/
+
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+struct Vector3f
+{
+    float x;
+    float y;
+    float z;
+
+    Vector3f() {}
+
+    Vector3f(float _x, float _y, float _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+};
+
+GLuint VBO;
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
+
+    glutSwapBuffers();
+}
+
+
+static void CreateVertexBuffer()
+{
+    glEnable(GL_CULL_FACE);
+    // glFrontFace(GL_CW);
+    // glCullFace(GL_FRONT);
+    Vector3f Vertices[3];
+    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f); // bottom left
+    Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f); // bottom right
+    Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f); // top
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
+
+    int width = 1024;
+    int height = 600;
+    glutInitWindowSize(width, height);
+
+    int x = 200;
+    int y = 100;
+    glutInitWindowPosition(x, y);
+    int win = glutCreateWindow("tutorial-3");
+    printf("window id: %d\n", win);
+
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
+    glClearColor(Red, Green, Blue, Alpha);
+
+    CreateVertexBuffer();
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glutMainLoop();
+
+    return 0;
+}
