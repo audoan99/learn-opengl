bf7219c59ca7ca6fe7f6b268a2ca1fd11575b5ae
Au Doan Ngoc <Au.DoanNgoc@vn.bosch.com>
2022-12-26 17:48:31 +0700

    update learning

---
+++ vertex_attribute_interpolation/main.cpp
@@ -0,0 +1,267 @@
+/*
+ *  Copyright 2022 Au Doan Ngoc
+ *
+ *  Author: Au Doan Ngoc
+ *  Date: Dec 21, 2022
+ *  Describe: Rotation Transformation
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+struct Vector3f
+{
+    float x;
+    float y;
+    float z;
+
+    Vector3f() {}
+
+    Vector3f(float _x, float _y, float _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+};
+
+class Matrix4f
+{
+public:
+    float m[4][4];
+
+    Matrix4f(){}
+
+    Matrix4f(float a00, float a01, float a02, float a03,
+             float a10, float a11, float a12, float a13,
+             float a20, float a21, float a22, float a23,
+             float a30, float a31, float a32, float a33)
+    {
+        m[0][0] = a00; m[0][1] = a01; m[0][2] = a02; m[0][3] = a03;
+        m[1][0] = a10; m[1][1] = a11; m[1][2] = a12; m[1][3] = a13;
+        m[2][0] = a20; m[2][1] = a21; m[2][2] = a22; m[2][3] = a23;
+        m[3][0] = a30; m[3][1] = a31; m[3][2] = a32; m[3][3] = a33;
+    }
+};
+
+bool ReadFile(const char* pFileName, std::string& outFile)
+{
+    std::ifstream f(pFileName);
+
+    bool ret = false;
+
+    if (f.is_open())
+    {
+        std::string line;
+        while (getline(f, line))
+        {
+            outFile.append(line);
+            outFile.append("\n");
+        }
+
+        f.close();
+
+        ret = true;
+    }
+    else {
+        printf("Error read file!\n");
+        ret = false;
+    }
+
+    return ret;
+}
+
+GLuint VBO;
+GLint gRotationLocation;
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    static float Scale = 1.0f;
+    static float Scale2 = 1.0f;
+
+    static float Delta = 0.01f;
+    static float Delta2 = 0.01f;
+
+    Scale += Delta;
+    if((Scale > 1.0f) || (Scale < 0.0f))
+    {
+        Delta *= -1.0f;
+    }
+
+    Scale2 += Delta2;
+    if((Scale > 1.0f) || (Scale < -1.0f))
+    {
+        Delta2 *= -1.0f;
+    }
+
+    Matrix4f Rotation(Scale, 0.0f  , 0.0f,  Scale2*2,
+                      0.0f , Scale , 0.0f,  0.0f,
+                      0.0f , 0.0f  , Scale, 0.0f,
+                      0.0f , 0.0f  , 0.0f,  1.0f);
+
+    glUniformMatrix4fv(gRotationLocation, 1, GL_TRUE, &Rotation.m[0][0]);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
+
+    glutPostRedisplay();
+
+    glutSwapBuffers();
+
+    usleep(20000);
+}
+
+
+static void CreateVertexBuffer()
+{
+    glEnable(GL_CULL_FACE);
+    // glFrontFace(GL_CW);
+    // glCullFace(GL_FRONT);
+    Vector3f Vertices[3];
+    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f); // bottom left
+    Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f); // bottom right
+    Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f); // top
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType);
+
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    const GLchar* p[1];
+    p[0] = pShaderText;
+
+    GLint Lengths[1];
+    Lengths[0] = (GLint)strlen(pShaderText);
+
+    glShaderSource(ShaderObj, 1, p, Lengths);
+
+    glCompileShader(ShaderObj);
+
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+const char* pVSFileName = "shader.vs";
+const char* pFSFileName = "shader.fs";
+
+static void CompileShaders()
+{
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0) {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    std::string vs, fs;
+
+    if (!ReadFile(pVSFileName, vs)) {
+        exit(1);
+    };
+
+    AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
+
+    if (!ReadFile(pFSFileName, fs)) {
+        exit(1);
+    };
+
+    AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(ShaderProgram);
+
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    gRotationLocation = glGetUniformLocation(ShaderProgram, "gRotation");
+    if(gRotationLocation == -1)
+    {
+        printf("Error getting uniform location of 'gScale'\n");
+        exit(1);
+    }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
+
+    int width = 1024;
+    int height = 600;
+    glutInitWindowSize(width, height);
+
+    int x = 200;
+    int y = 100;
+    glutInitWindowPosition(x, y);
+    int win = glutCreateWindow("tutorial");
+    printf("window id: %d\n", win);
+
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
+    glClearColor(Red, Green, Blue, Alpha);
+
+    CreateVertexBuffer();
+
+    CompileShaders();
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glutMainLoop();
+
+    return 0;
+}
